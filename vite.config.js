813414445d072a8f24c1543b7384e2ecec8ffbba
zhong <innovateandworld@163.com>
2026-03-18 10:44:46 +0800

    Add GitHub Actions deployment

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/work-timer/', // 例如：'/work-timer/'
+  base: '/work-timer/', // 改成你的仓库名
   build: {
     outDir: 'dist',
-    assetsDir: 'static'
+    assetsDir: 'static',
+    sourcemap: false,
+    minify: 'terser'
   }
 })
